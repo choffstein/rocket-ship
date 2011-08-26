e96c97f7f95f5ef59a4d2ea4e45b1673733ee05e
Corey M. Hoffstein <corey@newfoundresearch.com>
2011-08-26 10:08:33 -0400

    pushed clojure code

---
+++ src/rocket_ship/core.clj
@@ -0,0 +1,1 @@
+(ns rocket-ship.core)
---
+++ test/rocket_ship/test/core.clj
@@ -0,0 +1,6 @@
+(ns rocket-ship.test.core
+  (:use [rocket-ship.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
